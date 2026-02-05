36b95726ed049d2be75f65396645ec19e60057df
elvatix <kevin@elvatix.com>
2026-02-05 19:37:57 +0000

    Fix Hero component filename casing

--- nextjs/app/page.tsx
+++ nextjs/app/page.tsx
@@ -2,11 +2,8 @@ import Hero from "@/components/sections/Hero";
 
 export default function Home() {
   return (
-    <main>
-      <Hero
-        title="Welkom op onze nieuwe site"
-        subtitle="Next.js frontend, veilig naast Bubble"
-      />
-    </main>
+    <>
+      <Hero />
+    </>
   );
 }
