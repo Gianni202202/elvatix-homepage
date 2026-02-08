4c15ea1f1e4cb41d5eac4270d89d54f7b763d7f9
elvatix <kevin@elvatix.com>
2026-02-08 19:22:47 +0000

    Remove duplicate footer from page

--- nextjs/app/page.tsx
+++ nextjs/app/page.tsx
@@ -1,7 +1,6 @@
 import Hero from "@/components/sections/Hero";
 import Features from "@/components/sections/Features";
 import CTA from "@/components/sections/CTA";
-import Footer from "@/components/sections/Footer";
 
 export default function Home() {
   return (
@@ -9,7 +8,6 @@ export default function Home() {
       <Hero />
       <Features />
       <CTA />
-      <Footer />
     </>
   );
 }
